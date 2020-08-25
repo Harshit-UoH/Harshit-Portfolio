2e646378f631880a2ad163505f7c987259759a6e
Harshit UOH <harshit.uoh.18mcmc42@gmail.com>
2020-08-25 21:06:43 +0530

    Adding component ScrollToTop and changing Tab icon

--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
-import React, {Suspense , lazy} from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./css/style.css";
 import Loader from "./images/loader.gif";
 
 /* -------------- COMPONENTS --------------- */
 import Navbar from "./components/Navbar";
-
+import ScrollToTop from "./components/ScrollToTop";
 
 /* ------------- PAGES -------------- */
-const Home = lazy(() => import('./pages/Home'));
-const About = lazy(() => import('./pages/About'));
-const Resume = lazy(() => import('./pages/Resume'));
-const Portfolio = lazy(() => import('./pages/Portfolio'));
-const Contact = lazy(() => import('./pages/Contact'));
-const Admin = lazy(() => import('./pages/Admin'));
-const Error = lazy(() => import('./pages/Error'));
-
-
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Resume = lazy(() => import("./pages/Resume"));
+const Portfolio = lazy(() => import("./pages/Portfolio"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Admin = lazy(() => import("./pages/Admin"));
+const Error = lazy(() => import("./pages/Error"));
 
 function App() {
   return (
-<>
-        <Router>
-          <Navbar />
-          <Route render = {({location}) => (
-            <Suspense fallback = {<div className = "loader"><img src = {Loader} alt = "loader" /></div>}>
-                <Switch location = {location}>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/Home" component={Home} />
-                    <Route exact path="/About" component={About} />
-                    <Route exact path="/Resume" component={Resume} />
-                    <Route exact path="/Portfolio" component={Portfolio} />
-                    <Route exact path="/Contact" component={Contact} />
-                    <Route exact path="/Admin" component={Admin} />
-                    <Route component={Error} />
-                </Switch>
-                </Suspense>
-           ) }/>        
-        </Router>
-      </>
+    <>
+      <Router>
+        <ScrollToTop />
+        <Navbar />
+        <Route
+          render={({ location }) => (
+            <Suspense
+              fallback={
+                <div className="loader">
+                  <img src={Loader} alt="loader" />
+                </div>
+              }
+            >
+              <Switch location={location}>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/Home" component={Home} />
+                <Route exact path="/About" component={About} />
+                <Route exact path="/Resume" component={Resume} />
+                <Route exact path="/Portfolio" component={Portfolio} />
+                <Route exact path="/Contact" component={Contact} />
+                <Route exact path="/Admin" component={Admin} />
+                <Route component={Error} />
+              </Switch>
+            </Suspense>
+          )}
+        />
+      </Router>
+    </>
   );
 }
 
