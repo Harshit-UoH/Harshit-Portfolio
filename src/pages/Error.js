6cf172a943178b0f1553a28b048f7c36ab03163f
Harshit UOH <harshit.uoh.18mcmc42@gmail.com>
2020-08-25 21:00:18 +0530

    Completed Error Page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 import "../css/styleError.css";
 
 const Error = () => {
   return (
     <>
       <div className = "page">
-        <div className = "pageposition">
-          <h1>Error</h1>
+        <div className = "pageposition fullheight ">
+          <center className="styling mt-5">
+            <h1>ERROR ..!</h1>
+            <h2>404 PAGE NOT FOUND</h2>
+            <Link to="/">
+              <button className="btn btn-primary button">Back To Home</button>
+            </Link>
+          </center>
         </div>
       </div>
     </>
